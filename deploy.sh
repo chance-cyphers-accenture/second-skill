a67b896e847fb579805403a9cff3c5aac10baeea
chance.a.cyphers <chance.a.cyphers@gmail.com>
2019-08-02 15:26:35 -0400

    Updating pipeline

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,9 @@
 echo "ENV: $1"
 
 mkdir ~/.aws
-mkdir ~/.ask
+
+echo "[ask_cli_default]" > ~/.aws/credentials
+echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
+echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
+
+ask deploy
