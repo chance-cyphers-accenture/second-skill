ecdc7f00d9b88fac0ec24aa122e05ca89e14253d
chance.a.cyphers <chance.a.cyphers@gmail.com>
2019-08-02 15:37:18 -0400

    Fix everything

--- deploy.sh
+++ deploy.sh
@@ -6,17 +6,14 @@ echo "ENV: $1"
 mkdir ~/.aws
 
 echo "[ask_cli_default]" > ~/.aws/credentials
-
-echo "aws_access_key_id=" $AWS_ACCESS_KEY_ID
 echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
-
-echo "aws_secret_access_key=" $AWS_SECRET_ACCESS_KEY
 echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
 
 
 
 # Create ASK config
 
+mkdir ~/.ask
 touch ~/.aws/config
 
 sed -e s/ASK_ACCESS_TOKEN/${ASK_ACCESS_TOKEN}/g -e \
