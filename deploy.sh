5374219d5f73c8c0cff39e83f9cb0bf674f47b2c
chance.a.cyphers <chance.a.cyphers@gmail.com>
2019-08-02 15:35:36 -0400

    Fix everything

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 echo "ENV: $1"
 
+
+# Create AWS config
+
 mkdir ~/.aws
 
 echo "[ask_cli_default]" > ~/.aws/credentials
@@ -10,6 +13,11 @@ echo "aws_access_key_id=$AWS_ACCESS_KEY_ID" >> ~/.aws/credentials
 echo "aws_secret_access_key=" $AWS_SECRET_ACCESS_KEY
 echo "aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" >> ~/.aws/credentials
 
+
+
+# Create ASK config
+
+touch ~/.aws/config
 
 sed -e s/ASK_ACCESS_TOKEN/${ASK_ACCESS_TOKEN}/g -e \
     s/ASK_REFRESH_TOKEN/${ASK_REFRESH_TOKEN}/g conf/ask_cli.json > ~/.ask/cli_config
